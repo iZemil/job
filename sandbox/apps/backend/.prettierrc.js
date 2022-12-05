1d977b180eda0658c8d687045594932781d94d52
zemil <3zemil@gmail.com>
2022-12-05 16:27:07 +0300

    fix: delete waste

--- sandbox/apps/backend/.prettierrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	...require('../../.prettierrc'),
-	importOrderParserPlugins: ['typescript', 'decorators-legacy'],
-};
--- sandbox/apps/backend/src/express/router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/about', (req, res) => {
-	res.send('About birds');
-});
-
-export { router };
