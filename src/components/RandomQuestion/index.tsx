1d7b7d5b2ec63d907b26d925d2360c64ad11c2e0
zemil <3zemil@gmail.com>
2025-11-11 11:25:59 +0300

    feat: init favorite questions

--- src/components/RandomQuestion/index.tsx
+++ src/components/RandomQuestion/index.tsx
@@ -4,7 +4,7 @@ import clsx from 'clsx';
 import queryString from 'query-string';
 import React, { useEffect } from 'react';
 
-import { ANY_TOPIC, Question } from './question';
+import { Question, Store, TOPIC_SELECT } from './question';
 
 import useIsBrowser from '@docusaurus/useIsBrowser';
 import styles from './styles.module.css';
@@ -14,7 +14,7 @@ export const RandomQuestion = () => {
 	const history = useHistory();
 	const location = useLocation();
 	const parsedSearch: { topic?: string } = queryString.parse(location.search);
-	const [topic, setTopic] = React.useState<string>(parsedSearch?.topic || ANY_TOPIC);
+	const [topic, setTopic] = React.useState<string>(parsedSearch?.topic || TOPIC_SELECT.ANY);
 	const [question, setQuestion] = React.useState<Question>();
 
 	useEffect(() => {
@@ -35,31 +35,61 @@ export const RandomQuestion = () => {
 	};
 
 	useEffect(() => {
-		if (topic !== ANY_TOPIC) {
+		if (topic !== TOPIC_SELECT.ANY) {
 			setQuestion(Question.random(topic, false));
 		}
 	}, [topic]);
+
+	const handleBookmark = (question: Question) => {
+		Question.handleBookmark(question);
+	};
+
+	const isFavorite = Question.isFavorite(question);
+	const favorites = Store.getFavorites();
 
 	return (
 		<div className={styles.container}>
 			<h2 className={styles.title}>
 				Question Topic
 				<select className={styles.topic} value={topic} onChange={handleChangeTopic}>
+					<option value={TOPIC_SELECT.ANY}>All</option>
+					{favorites.length && <option value={TOPIC_SELECT.FAVORITE}>Favorites</option>}
 					{Question.topics().map((topic) => (
 						<option key={topic}>{topic}</option>
 					))}
-					<option value={ANY_TOPIC}>All</option>
 				</select>
 			</h2>
 
-			{question && topic === ANY_TOPIC && (
+			{question && topic === TOPIC_SELECT.ANY && (
 				<Link className={styles.topicLink} to={Question.getTopicLink(question)}>
 					#{Question.extractTopicType(question)}
 				</Link>
 			)}
 
 			<div className={styles.question}>
-				{question && <h3>{question.title}</h3>}
+				{question && (
+					<h3>
+						{question.title}{' '}
+						<svg
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+							className={clsx(styles.bookmark, {
+								[styles.bookmarkActive]: isFavorite,
+							})}
+							onClick={() => handleBookmark(question)}
+						>
+							<path
+								fillRule="evenodd"
+								clipRule="evenodd"
+								d="M19 20H17.1717L12.7072 15.5354C12.3166 15.1449 11.6835 15.1449 11.2929 15.5354L6.82843 20L5 20V7C5 5.34315 6.34315 4 8 4H16C17.6569 4 19 5.34314 19 7V20ZM17 7C17 6.44772 16.5523 6 16 6H8C7.44772 6 7 6.44772 7 7V17L9.87873 14.1212C11.0503 12.9497 12.9498 12.9497 14.1214 14.1212L17 16.9999V7Z"
+								fill="currentColor"
+							/>
+						</svg>
+					</h3>
+				)}
 
 				{question && (
 					<Link target="blank" to={Question.getLink(question)}>
--- src/components/RandomQuestion/question.ts
+++ src/components/RandomQuestion/question.ts
@@ -18,10 +18,15 @@ function random<T>(arr: T[]): T {
 	return arr[Math.floor(Math.random() * arr.length)];
 }
 
-export const ANY_TOPIC = 'any';
+export const TOPIC_SELECT = {
+	ANY: 'any',
+	FAVORITE: 'favorite',
+} as const;
 
 export class Store {
 	public static KEY = 'questionStore';
+
+	public static FAVORITES = 'favorites';
 
 	public static getCache(): { [topicTitle: string]: string[] } {
 		return ls.get(Store.KEY) ?? {};
@@ -60,6 +65,12 @@ export class Store {
 			});
 		}
 	}
+
+	public static getFavorites(): string[] {
+		const favorites = ls.get<string[]>(Store.FAVORITES);
+
+		return favorites || [];
+	}
 }
 
 export class Question {
@@ -73,9 +84,34 @@ export class Question {
 		return topics.map((it) => it.title);
 	}
 
+	public static fromRaw(question: IQuestion, topic: ITopicData): Question {
+		return {
+			id: question.id,
+			title: question.title,
+			topicTitle: topic.dir,
+			topicLen: topic.data.length,
+		};
+	}
+
 	public static random(topicTitle: string, withCache = false): Question {
+		if (topicTitle === TOPIC_SELECT.FAVORITE) {
+			const ids = Store.getFavorites();
+			const questionsByIds: Question[] = [];
+			// TODO: refactor
+			topics.forEach((t) => {
+				t.data.forEach((q) => {
+					if (ids.includes(q.id)) {
+						questionsByIds.push(Question.fromRaw(q, t));
+					}
+				});
+			});
+
+			// TODO: добавить очередь, а не рандом
+			return random(questionsByIds);
+		}
+
 		let topic = topics.find((it) => it.title === topicTitle);
-		if (!topic || topicTitle === ANY_TOPIC) {
+		if (!topic || topicTitle === TOPIC_SELECT.ANY) {
 			topic = random(topics);
 		}
 
@@ -111,5 +147,37 @@ export class Question {
 
 	public static getTopicLink(q: Question): string {
 		return `/${q.topicTitle}`;
+	}
+
+	/**
+	 *
+	 * @returns true if favorite was added and false for removed
+	 */
+	public static handleBookmark(q: Question): boolean {
+		const favorites = Store.getFavorites();
+
+		const isFavorite = favorites.find((f) => f === q.id);
+
+		if (isFavorite) {
+			const filtered = favorites.filter((f) => f !== q.id);
+
+			ls.set(Store.FAVORITES, filtered);
+
+			return false;
+		}
+
+		ls.set(Store.FAVORITES, [...favorites, q.id]);
+
+		return true;
+	}
+
+	public static isFavorite(q?: Question): boolean {
+		if (!q) {
+			return false;
+		}
+
+		const favorites = Store.getFavorites();
+
+		return favorites.find((f) => f === q.id) !== undefined;
 	}
 }
