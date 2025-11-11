e6cadb2634d102731f78bc9b505df1ce3636eab1
zemil <3zemil@gmail.com>
2025-11-11 11:58:05 +0300

    feat: UI

--- src/components/RandomQuestion/index.tsx
+++ src/components/RandomQuestion/index.tsx
@@ -50,7 +50,7 @@ export const RandomQuestion = () => {
 	return (
 		<div className={styles.container}>
 			<h2 className={styles.title}>
-				Question Topic
+				Questions
 				<select className={styles.topic} value={topic} onChange={handleChangeTopic}>
 					<option value={TOPIC_SELECT.ANY}>All</option>
 					{favorites.length && <option value={TOPIC_SELECT.FAVORITE}>Favorites</option>}
@@ -69,41 +69,52 @@ export const RandomQuestion = () => {
 			<div className={styles.question}>
 				{question && (
 					<h3>
-						{question.title}{' '}
-						<svg
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-							className={clsx(styles.bookmark, {
-								[styles.bookmarkActive]: isFavorite,
-							})}
+						{question.title}
+						<button
+							className={clsx('button button--sm', styles.bookmarkBtn)}
 							onClick={() => handleBookmark(question)}
 						>
-							<path
-								fillRule="evenodd"
-								clipRule="evenodd"
-								d="M19 20H17.1717L12.7072 15.5354C12.3166 15.1449 11.6835 15.1449 11.2929 15.5354L6.82843 20L5 20V7C5 5.34315 6.34315 4 8 4H16C17.6569 4 19 5.34314 19 7V20ZM17 7C17 6.44772 16.5523 6 16 6H8C7.44772 6 7 6.44772 7 7V17L9.87873 14.1212C11.0503 12.9497 12.9498 12.9497 14.1214 14.1212L17 16.9999V7Z"
-								fill="currentColor"
-							/>
-						</svg>
+							<svg
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+								className={clsx(styles.bookmarkIcon, {
+									[styles.bookmarkActive]: isFavorite,
+								})}
+							>
+								<path
+									fillRule="evenodd"
+									clipRule="evenodd"
+									d="M19 20H17.1717L12.7072 15.5354C12.3166 15.1449 11.6835 15.1449 11.2929 15.5354L6.82843 20L5 20V7C5 5.34315 6.34315 4 8 4H16C17.6569 4 19 5.34314 19 7V20ZM17 7C17 6.44772 16.5523 6 16 6H8C7.44772 6 7 6.44772 7 7V17L9.87873 14.1212C11.0503 12.9497 12.9498 12.9497 14.1214 14.1212L17 16.9999V7Z"
+									fill="currentColor"
+								/>
+							</svg>
+						</button>
 					</h3>
 				)}
 
-				{question && (
+				{/* {question && (
 					<Link target="blank" to={Question.getLink(question)}>
 						See answer
 					</Link>
-				)}
+				)} */}
 			</div>
 
-			<button
-				className={clsx('button button--secondary button--lg', styles.nextButton)}
-				onClick={handleClickNext}
-			>
-				🎲 Next
-			</button>
+			<div className={styles.actionBar}>
+				{question && (
+					<button className={clsx('button button--outline button--lg')}>
+						<Link className={styles.seeAnswerBtn} target="blank" to={Question.getLink(question)}>
+							See answer
+						</Link>
+					</button>
+				)}
+
+				<button className={clsx('button button--secondary button--lg')} onClick={handleClickNext}>
+					<span>🎲</span>Next
+				</button>
+			</div>
 		</div>
 	);
 };
